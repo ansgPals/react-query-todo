c4caf341dfa4c0030722cc3c686366ad003a87f2
hyemin-mun <hyemin.mun@illuminarean.com>
2023-05-10 21:10:59 +0900

    feat: Setting , Login 구현

--- client/src/pages/todo/index.tsx
+++ client/src/pages/todo/index.tsx
@@ -1,3 +1,20 @@
+import { useReactQuery } from "@/api/http";
+import axios from "@/api/middlewares";
+
+import { TODO_LIST_API_URL } from "@/constants/endpoint";
+
 export default function TodoList() {
-  return <></>;
+  const { data, isLoading, refetch } = useReactQuery({
+    url: TODO_LIST_API_URL,
+  });
+
+  console.log(data);
+
+  return isLoading ? (
+    <> 로딩중 </>
+  ) : (
+    <>
+      <button onClick={refetch}>리패치!!</button>
+    </>
+  );
 }
---
+++ client/src/types/interface.ts
@@ -0,0 +1,15 @@
+export interface ILoginData {
+  message: string
+  token: string
+}
+export interface IFormValues {
+  email?: string
+  password?: string
+}
+export interface ITodoItemData {
+  title: string
+  content: string
+  id: string
+  createdAt: Date
+  updatedAt: Date
+}
